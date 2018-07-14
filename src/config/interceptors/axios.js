6e72d1e61568f10505652341edac9e2b62005dbf
suchang <suchang@zhihu.com>
2018-07-14 15:20:18 +0800

    init

--- src/config/interceptors/axios.js
+++ src/config/interceptors/axios.js
@@ -4,14 +4,14 @@ export function requestSuccessFunc (requestObj) {
     CONSOLE_REQUEST_ENABLE && console.info('requestInterceptorFunc', `url: ${requestObj.url}`, requestObj)
     // 自定义请求拦截逻辑，可以处理权限，请求发送监控等
     // ...
-    
+
     return requestObj
 }
 
 export function requestFailFunc (requestError) {
     // 自定义发送请求失败逻辑，断网，请求发送监控等
     // ...
-    
+
     return Promise.reject(requestError);
 }
 
@@ -24,19 +24,19 @@ export function responseSuccessFunc (responseObj) {
     //     msg: 'this is a msg',
     //     data: null
     // }
-    
+
     let resData =  responseObj.data
     let {code} = resData
-    
+
     switch(code) {
-        case 0: // 如果业务成功，直接进成功回调  
+        case 0: // 如果业务成功，直接进成功回调
             return resData.data;
-        case 1111: 
+        case 1111:
             // 如果业务失败，根据不同 code 做不同处理
             // 比如最常见的授权过期跳登录
             // 特定弹窗
             // 跳转特定页面等
-            
+
             location.href = xxx // 这里的路径也可以放到全局配置里
             return;
         default:
@@ -49,5 +49,6 @@ export function responseSuccessFunc (responseObj) {
 export function responseFailFunc (responseError) {
     // 响应失败，可根据 responseError.message 和 responseError.response.status 来做监控处理
     // ...
+
     return Promise.reject(responseError);
 }
