90615eafc74cecba192b53412f11ce0a9cfd1ee8
yiran7324 <yiran7324@gmail.com>
2018-07-18 23:14:30 +0800

    basic framework

--- src/views/FormMetaEditor/index.vue
+++ src/views/FormMetaEditor/index.vue
@@ -1,37 +1,73 @@
 <template>
-  <h1>这是问卷编辑</h1>
+  <div style="text-align: center; width: 600px">
+    <!-- Whole -->
+    <h1>Form Editor</h1>
+    <div>
+      <!-- Form head -->
+      <div>
+        <h1>Name: </h1>
+        <Input v-model="item_type_name"
+               placeholder="Can't assign to none string..."
+               style="width: 400px">
+
+        </Input>
+        <br>
+        <h1>Signal: </h1>
+        <Input v-model="item_type_id"
+               placeholder="Can't assign to none string..."
+               style="width: 400px">
+
+        </Input>
+      </div>
+      <!-- head End -->
+
+      <!-- Form floater -->
+      <div>
+
+        <p>Here add a item</p>
+        <Row>
+          <Button type="primary"
+                  @click="addFloater">Click to add floater</Button>
+        </Row>
+
+        <div style="float: right">
+          <Row>
+            <Button type="primary"
+                    @click="addFloater"
+                    style="width: 100px">Add</Button>
+          </Row>
+          <Row>
+            <Button type="primary"
+                    @click="addFloater"
+                    style="width: 100px">Edit</Button>
+          </Row>
+          <Row>
+            <Button type="primary"
+                    @click="addFloater"
+                    style="width: 100px">Delete</Button>
+          </Row>
+        </div>
+
+      </div>
+      <!-- floater End -->
+    </div>
+  </div>
 </template>
+
 <script>
-  import { getAllFormMetas, getFormMeta } from '../../service/api/dqs'
+  // import
   export default {
-    data () {
+    data() {
       return {
-        columns1: [
-          {
-            title: 'Name',
-            key: 'name'
-          },
-          {
-            title: 'Age',
-            key: 'age'
-          },
-          {
-            title: 'Address',
-            key: 'address'
-          }
-        ],
-        data1: [
-          {
-            name: 'John Brown',
-            age: 18,
-            address: 'New York No. 1 Lake Park',
-            date: '2016-10-03'
-          }
-        ]
+        item_type_name: 'name',
+        item_type_id: 'signal'
+
       }
     },
-    mounted () {
-
+    methods: {
+      addFloater() {
+        this.$Message.info('Adjusting button')
+      }
     }
   }
 </script>
