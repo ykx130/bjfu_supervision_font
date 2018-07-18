62f5b6d8a611afe7c5fd2c5280ee5f7dae905ca3
LunaHemx <HeMingxu@bjfu.edu.cn>
2018-07-18 10:56:33 +0800

    Show meta of form_meta

--- src/views/FormShow/index.vue
+++ src/views/FormShow/index.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
-    <h1 v-model="form_meta.meta.table_name" style="text-align: center">{{ form_meta.meta.table_name }}</h1>
+  <div :data="form_meta">
+    <h1 style="text-align: center">{{ form_meta.meta.table_name }}</h1>
     <br>
     <div>
-      <div v-model="form_meta.meta.created_at" v-bind:style="{ fontSize:'15px',float:'left',width:'33%' }">创建时间：{{ form_meta.meta.created_at }}</div>
-      <div v-model="form_meta.meta.created_by" v-bind:style="{ fontSize:'15px',float:'left',width:'33%' }">创建人：{{ form_meta.meta.created_by }}</div>
-      <div v-model="form_meta.meta.version" v-bind:style="{ fontSize:'15px',float:'left',width:'33%' }">版本号：{{ form_meta.meta.version }}</div>
+      <div v-bind:style="{ fontSize:'15px',float:'left',width:'33%' }">创建时间：{{ form_meta.meta.created_at }}</div>
+      <div v-bind:style="{ fontSize:'15px',float:'left',width:'33%' }">创建人：{{ form_meta.meta.created_by }}</div>
+      <div v-bind:style="{ fontSize:'15px',float:'left',width:'33%' }">版本号：{{ form_meta.meta.version }}</div>
     </div>
   </div>
 </template>
 <script>
-  import { getFormMeta } from "../../service/api/dqs";
+  import { getFormMeta } from "../../service/api/dqs"
 
   export default {
     data () {
@@ -41,7 +41,7 @@
       }
     },
     mounted() {
-      getFormMeta(1).then((resp)=>{console.log(resp)})
+      getFormMeta(1).then((resp)=>{this.form_meta=resp.data.form_meta})
     }
   }
 </script>
