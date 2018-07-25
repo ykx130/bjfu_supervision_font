c68b2344398e287f72188da5032776f7475375ed
LunaHemx <HeMingxu@bjfu.edu.cn>
2018-07-25 09:14:00 +0800

    问卷填写1.0

--- src/views/FormShow/index.vue
+++ src/views/FormShow/index.vue
@@ -1,13 +1,23 @@
 <template>
   <div :data="form_meta">
     <h1 style="text-align: center">{{ form_meta.meta.table_name }}</h1>
-    <br>
+    <br/>
     <div>
       <div v-bind:style="{ fontSize:'15px',float:'left',width:'33%' }">创建时间：{{ form_meta.meta.created_at }}</div>
       <div v-bind:style="{ fontSize:'15px',float:'left',width:'33%' }">创建人：{{ form_meta.meta.created_by }}</div>
       <div v-bind:style="{ fontSize:'15px',float:'left',width:'33%' }">版本号：{{ form_meta.meta.version }}</div>
     </div>
-    <br>
+    <br/>
+    <div>
+      <!--<Select label="课程名称" v-model="form.meta.lesson" v-bind:style="{ fontSize:'15px',width:'400px' }"></Select>
+      <Select label="课程内容" v-model="form.meta.content" v-bind:style="{ fontSize:'15px',width:'400px' }"></Select>
+      <Select label="课程属性" v-model="form.meta.lesson_attr" v-bind:style="{ fontSize:'15px',width:'400px' }"></Select>
+      <br/>-->
+      <Input placeholder="授课教师" v-model="form.meta.teacher" v-bind:style="{ fontSize:'15px',width:'400px' }"></Input>
+      <Input placeholder="上课班级" v-model="form.meta.class" v-bind:style="{ fontSize:'15px',width:'400px' }"></Input>
+      <Input placeholder="上课地点" v-model="form.meta.place" v-bind:style="{ fontSize:'15px',width:'400px' }"></Input>
+    </div>
+    <br/>
     <div v-for="it in form_meta.items">
       <div v-if="it.item_type === 'sub_title_block_start'">
         <h1 style="height: 80px;line-height: 80px;margin-left: 20px">{{ it.payload.title }}</h1>
@@ -15,14 +25,22 @@
       <div v-if="it.item_type === 'radio_options'">
         <h1 style="height: 70px;line-height: 70px;margin-left: 20px">{{ it.extra }}</h1>
         <RadioGroup>
-          <Radio v-for="op in it.payload.options" :label="op.label" :key="op.value" v-bind:style="{ fontSize:'15px',marginLeft:'25px' }">
+          <Radio v-model="form.values.value" v-for="op in it.payload.options" :label="op.label" :key="op.value" v-bind:style="{ fontSize:'15px',marginLeft:'25px' }">
             <span>{{op.label}}</span>
           </Radio>
         </RadioGroup>
       </div>
+      <div v-if="it.item_type === 'checkbox_options'">
+        <h1 style="height: 70px;line-height: 70px;margin-left: 20px">{{ it.extra }}</h1>
+        <CheckboxGroup>
+          <Checkbox v-model="form.values.value" v-for="op in it.payload.options" :label="op.label" :key="op.value" v-bind:style="{ fontSize:'15px',marginLeft:'25px' }">
+            <span>{{op.label}}</span>
+          </Checkbox>
+        </CheckboxGroup>
+      </div>
       <div v-if="it.item_type === 'raw_text'">
         <h1 style="height: 70px;line-height: 70px;margin-left: 20px">{{ it.extra }}</h1>
-        <Input v-model="it.payload.options" type="textarea" placeholder="Satisfation about teachers..." v-bind:style="{marginLeft:'25px',width:'85%'}"></Input>
+        <Input v-model="form.values.value" type="textarea" placeholder="Satisfation about teachers..." v-bind:style="{marginLeft:'25px',width:'85%'}"></Input>
       </div>
       <div v-if="it.item_type === 'sub_title_block_end'">
         <h1 style="height: 80px;line-height: 80px;margin-left: 20px">{{ it.payload.opitons }}</h1>
@@ -60,13 +78,30 @@
               }
             }
           ]
+        },
+        form: {
+          meta_table_id:"",
+          meta: {
+            content:"",
+            lesson:"",
+            lesson_attr:"",
+            teacher:"",
+            class:"",
+            place:""
+          },
+          values: [
+            {
+              item_id:"",
+              item_type:"",
+              item_name:"",
+              value:""
+            }
+          ]
         }
       }
     },
     mounted() {
       let id=this.$route.params.id;
-      //let id='http://localhost:8080/#/form_show/:id'.split('//')[1].split('/')[3];
-      console.log(id);
       getFormMeta(id).then(
         (resp)=>{
           this.form_meta=resp.data.form_meta
