80b3de93f911c92a16f9775bf33fe148a6ba0be5
lyric777 <wysxxuzhili48@163.com>
2018-07-30 16:02:17 +0800

    FormManager初版

--- src/service/api/dqs.js
+++ src/service/api/dqs.js
@@ -2,12 +2,20 @@ import axios from 'axios'
 
 export const getAllFormMetas =(params)=> {
   return axios.get('/api/form_metas', {params: params})
-}
+};
 
 export const getFormMeta = params =>{
   return axios.get('/api/form_metas/'+ params)
-}
+};
 
 export const handleDeleteFormMetas =(params)=>{
   return axios.delete('/api/form_metas/' + params)
-}
+};
+
+export const getForms =(params)=> {
+  return axios.get('/api/forms', {params: params})
+};
+
+export const handleDeleteForms =(params)=>{
+  return axios.delete('/api/forms/' + params)
+};
