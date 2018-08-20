c6b2958c3324af859d4374e1f75508a7287c7890
suchang <1103232282@qq.com>
2018-08-20 20:29:11 +0800

    提交代码

--- src/config/interceptors/router.js
+++ src/config/interceptors/router.js
@@ -1,7 +1,19 @@
+import store from 'Plugins/store'
+import {currentUser} from '../../service/api/user'
+
 export function routerBeforeEachFunc (to, from, next) {
     // 这里可以做页面拦截，很多后台系统中也非常喜欢在这里面做权限处理
-    if (to.path === '/'){
-      next({path: '/DashBordAdmin'})
+    if (to.path !== '/login') {
+      currentUser().then((resp)=>{
+        if (resp.status !== 200 ) {
+          next({path: "/login"})
+        } else {
+          store.commit('SET_USER_INFO', resp.data.user);
+          if (to.path === '/'){
+            next({path: '/DashBordAdmin'})
+          }
+        }
+        next()
+      })
     }
-    next()
 }
