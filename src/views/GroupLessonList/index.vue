f724a7d26f3284e2ea671e8db2d86152e5e5710f
LunaHemx <HeMingxu@bjfu.edu.cn>
2018-09-09 21:46:03 +0800

    各组情况表格2.0版

--- src/views/GroupLessonList/index.vue
+++ src/views/GroupLessonList/index.vue
@@ -3,126 +3,56 @@
     <Tabs :value="selected_group_name" @on-click="onTypeTabClick">
       <TabPane v-for="(item, index) in groups" :label="item.name" :name="item.name" :key="item.name + index"></TabPane>
     </Tabs>
+
+    <template v-if="selected_group_name==='第一组'">
+      <all-group></all-group>
+    </template>
+    <template v-else-if="selected_group_name==='第二组'">
+      <all-group></all-group>
+    </template>
   </Card>
 </template>
 
 <script>
-  import {queryLessons, putLesson} from '../../service/api/lesson'
-  import {queryTerms, getCurrentTerms} from '../../service/api/term'
+  import {queryGroupLesson, putLesson} from '../../service/api/lesson'
   import {queryGroups} from '../../service/api/user'
   import  FloatBar from '../../components/float_bar/float_bar'
+  import allGroup from './components/allGroup'
+
+
   export default {
-    components:{},
+    components:{
+      allGroup
+    },
     data: function() {
       return {
-
-        query: {}, // 查询用的参数
         total: 0, // 总数量
         data: [], //数据
-        terms: [],
         groups:[],
-        selected_group_name: '',
-        pages: {
-          _page: 1,
-          _per_page: 10
-        }, //分页
-        columns: [
-          {
-            title: '课程名字',
-            render: function (h, params) {
-              return (
-                <span>{ params.row.lesson_name }</span>
-            )
-            }
-          },
-          {
-            title: '课程属性',
-            render: function (h, params) {
-              return (
-                <span>{ params.row.lesson_attribute }</span>
-            )
-            }
-          },
-          {
-            title: '课程状态',
-            render: function (h, params) {
-              return (
-                <span>{ params.row.lesson_state }</span>
-            )
-            }
-          },
-          {
-            title: '课程级别',
-            render: function (h, params) {
-              return (
-                <span>{ params.row.lesson_level }</span>
-            )
-            }
-          },
-          {
-            title: '操作',
-            align: 'center',
-            render: (h, params) => {
-              return h('div', [
-                h('Button', {
-                  props: {
-                    type: 'primary',
-                    size: 'small'
-                  },
-                  style: {
-                    marginRight: '2px'
-                  },
-                  on: {
-                    click: () => {
-                      this.selected_lesson_id = params.row.id;
-                      this.showLessonProfileModal=true
-                    }
-                  }
-                }, '查看')
-              ]);
-            }
-          }
-        ]
+        selected_group_name: ''
       }
     },
     methods: {
       onTableChange(query, pages) {
         //数据表发生变化请求数据
         let args = {...query, ...pages};
-        queryLessons(args).then((resp)=>{
-          this.data = resp.data.lessons;
+        queryGroupLesson(args).then((resp)=>{
+          this.data = resp.data.lesson_records;
           this.total = resp.data.total;
         })
       },
-      onPageChange(page) {
-        //分页变化
-        this.pages._page = page;
-        this.onTableChange(this.query, this.pages)
-      },
-      onSearch() {
-        //查询变化
-        this.pages._page = 1;
-        this.onTableChange(this.query, this.pages)
-      },
       onTypeTabClick: function (name) {
         // 切换标签触发
-        this.selected_group_name = name
-        this.onTableChange(this.query, this.pages)
-      },
+        this.selected_group_name = name;
+        this.onTableChange(this.query, this.pages);
+        this.$router.push({path:'/dqs/lesson_records',query:{group_name: this.selected_group_name}})
+      }
     },
-    mounted: function () {
-      const args = this.$route.query;
-      queryTerms().then((resp)=>{
-        this.terms = resp.data.terms
-      });
+    mounted: function() {
       queryGroups().then((resp)=>{
-        this.groups = resp.data.groups
-        this.selected_group_name = this.groups[0].name
+        this.groups = resp.data.groups;
+        //this.selected_group_name = this.groups[0].name;
       })
-      getCurrentTerms().then((termResp)=>{
-        this.query.term = termResp.data.term.name;
-      });
-
     }
   }
 </script>
