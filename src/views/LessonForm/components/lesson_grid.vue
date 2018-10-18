bc099e0052fb8202b7caaf3b32fd0a7c8c499b4a
lyric777 <wysxxuzhili48@163.com>
2018-10-18 16:42:53 +0800

    LessonForm第四版

--- src/views/LessonForm/components/lesson_grid.vue
+++ src/views/LessonForm/components/lesson_grid.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-if="someLesson" v-for="lesson in someLesson">
-      <p>{{ lesson.lesson_name }},{{ lesson.id }},{{ lesson.lesson_class }}班,
+      <p>{{ lesson.lesson_name }}，{{ lesson.lesson_class }}班,
         <span v-for="(value, index) in lesson.week" v-if="index % 2 === 0">
           <span v-if="index !== lesson.week.length - 2">
             <span v-if="lesson.week[index] === lesson.week[index + 1]">
@@ -20,7 +20,6 @@
             </span>
           </span>
         </span>周</p>
-      <hr />
     </div>
   </div>
 </template>
--- src/views/LessonForm/index.vue
+++ src/views/LessonForm/index.vue
@@ -190,7 +190,6 @@
             }
             week.push(allWeeks[allWeeks.length - 1]);
           }
-          console.log(week);//
           return week;
         }
       },
@@ -208,56 +207,50 @@
               let time = this.lessons[lesson].lesson_cases[lesson_case].lesson_time;
               if (time.indexOf("0102") > -1) {
                 if (flag[0][weekday] === 0) {
-                  this.data[0][weekday].push(this.lessons[lesson]);
-                  console.log(this.data[0][weekday].length);
-                  this.data[0][weekday][this.data[0][weekday].length - 1].week = this.getWeek(lesson, weekday, '0102').slice(0);
-                  console.log(this.getWeek(lesson, weekday, '0102'));
-                  console.log(this.data[0][3]);//
+                  this.data[0][weekday].push(JSON.parse(JSON.stringify(this.lessons[lesson])));
+                  this.data[0][weekday][this.data[0][weekday].length - 1].week = this.getWeek(lesson, weekday, '0102');
                   flag[0][weekday] = 1;
                 }
               }
               if (time.indexOf("0304") > -1) {
                 if (flag[1][weekday] === 0) {
-                  this.data[1][weekday].push(this.lessons[lesson]);
-                  this.data[1][weekday][this.data[1][weekday].length - 1].week = this.getWeek(lesson, weekday, '0304').slice(0);
+                  this.data[1][weekday].push(JSON.parse(JSON.stringify(this.lessons[lesson])));
+                  this.data[1][weekday][this.data[1][weekday].length - 1].week = this.getWeek(lesson, weekday, '0304');
                   flag[1][weekday] = 1;
                 }
               }
               if (time.indexOf("05") > -1) {
                 if (flag[2][weekday] === 0) {
-                  this.data[2][weekday].push(this.lessons[lesson]);
-                  this.data[2][weekday][this.data[2][weekday].length - 1].week = this.getWeek(lesson, weekday, '05').slice(0);
+                  this.data[2][weekday].push(JSON.parse(JSON.stringify(this.lessons[lesson])));
+                  this.data[2][weekday][this.data[2][weekday].length - 1].week = this.getWeek(lesson, weekday, '05');
                   flag[2][weekday] = 1;
-                  console.log(this.getWeek(lesson, weekday, '05'));
-                  console.log(this.data[2][3]);
-                  console.log(this.data[0][3]);//
                 }
               }
               if (time.indexOf("0607") > -1) {
                 if (flag[3][weekday] === 0) {
-                  this.data[3][weekday].push(this.lessons[lesson]);
-                  this.data[3][weekday][this.data[3][weekday].length - 1].week = this.getWeek(lesson, weekday, '0607').concat();
+                  this.data[3][weekday].push(JSON.parse(JSON.stringify(this.lessons[lesson])));
+                  this.data[3][weekday][this.data[3][weekday].length - 1].week = this.getWeek(lesson, weekday, '0607');
                   flag[3][weekday] = 1;
                 }
               }
               if (time.indexOf("0809") > -1) {
                 if (flag[4][weekday] === 0) {
-                  this.data[4][weekday].push(this.lessons[lesson]);
-                  this.data[4][weekday][this.data[4][weekday].length - 1].week = this.getWeek(lesson, weekday, '0809').concat();
+                  this.data[4][weekday].push(JSON.parse(JSON.stringify(this.lessons[lesson])));
+                  this.data[4][weekday][this.data[4][weekday].length - 1].week = this.getWeek(lesson, weekday, '0809');
                   flag[4][weekday] = 1;
                 }
               }
               if (time.indexOf("1011") > -1) {
                 if (flag[5][weekday] === 0) {
-                  this.data[5][weekday].push(this.lessons[lesson]);
-                  this.data[5][weekday][this.data[5][weekday].length - 1].week = this.getWeek(lesson, weekday, '1011').concat();
+                  this.data[5][weekday].push(JSON.parse(JSON.stringify(this.lessons[lesson])));
+                  this.data[5][weekday][this.data[5][weekday].length - 1].week = this.getWeek(lesson, weekday, '1011');
                   flag[5][weekday] = 1;
                 }
               }
               if (time.indexOf("12") > -1) {
                 if (flag[6][weekday] === 0) {
-                  this.data[6][weekday].push(this.lessons[lesson]);
-                  this.data[6][weekday][this.data[6][weekday].length - 1].week = this.getWeek(lesson, weekday, '12').concat();
+                  this.data[6][weekday].push(JSON.parse(JSON.stringify(this.lessons[lesson])));
+                  this.data[6][weekday][this.data[6][weekday].length - 1].week = this.getWeek(lesson, weekday, '12');
                   flag[6][weekday] = 1;
                 }
               }
