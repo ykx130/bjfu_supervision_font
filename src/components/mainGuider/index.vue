6cff402a21d8b401171e196ee7e3fa3147175eae
yiran7324 <yiran7324@gmail.com>
2018-10-21 21:50:12 +0800

    done with highlight and router push

--- src/components/mainGuider/index.vue
+++ src/components/mainGuider/index.vue
@@ -34,21 +34,26 @@
   <div>
     <Layout>
       <Header>
-        <Menu  mode="horizontal" theme="light" active-name="1">
+        <Menu  mode="horizontal" theme="light" :active-name="activity_name" @on-select="onMenuSelect">
           <div class="layout-nav">
-          <MenuItem name="1">
+          <MenuItem name="judge">
             <Icon type="ios-paper" />
             问卷填写
           </MenuItem>
-          <MenuItem name="2">
+          <MenuItem name="attend">
             <Icon type="ios-people" />
             活动报名
           </MenuItem>
-          <MenuItem name="4">
+          <MenuItem name="consult_apply">
             <Icon type="ios-construct" />
             咨询申请
           </MenuItem>
           </div>
+          <user :user-avator="userAvator" style="float: right; margin-right: 40px"/>
+          <userNotices style="margin-right: 10px; float: right;"></userNotices>
+          <language v-if="$config.useI18n" @on-lang-change="setLocal" style="margin-right: 10px; float: right;" :lang="local"/>
+          <error-store v-if="$config.plugin['error-store'] && $config.plugin['error-store'].showInHeader" :has-read="hasReadErrorPage" :count="errorCount" style="float: right; margin-right: 10px"></error-store>
+          <fullscreen v-model="isFullscreen" style="margin-right: 10px; float: right"/>
         </Menu>
       </Header>
       <Content :style="{padding: '20px 10px', height: '680px'}">
@@ -59,108 +64,128 @@
   </div>
 </template>
 <script>
-  import HeaderBar from '_c/main/components/header-bar'
-  import TagsNav from 'c/main/components/tags-nav'
-  import User from 'c/main/components/user'
-  import Fullscreen from 'c/main/components/fullscreen'
-  import Language from 'c/main/components/language'
+  // import HeaderBar from '_c/main/components/header-bar'
+  // import TagsNav from 'c/main/components/tags-nav'
+  import User from '_c/mainGuider/components/user'
+  import Fullscreen from '_c/mainGuider/components/fullscreen'
+  import Language from '_c/mainGuider/components/language'
   import userNotices from '_c/user_notices'
-  import ErrorStore from 'c/main/components/error-store'
-  import { mapMutations, mapActions, mapGetters } from 'vuex'
-  import minLogo from '@/assets/images/logo-min.jpg'
-  import maxLogo from '@/assets/images/logo.jpg'
-  import { getNewTagList, getNextRoute, routeEqual } from '@/libs/util'
+  import ErrorStore from '_c/mainGuider/components/error-store'
+  // import { mapMutations, mapActions, mapGetters } from 'vuex'
+  // import minLogo from '@/assets/images/logo-min.jpg'
+  // import maxLogo from '@/assets/images/logo.jpg'
+  // import { getNewTagList, getNextRoute, routeEqual } from '@/libs/
+
 
   export default {
       name: 'mainGuider',
       components: {
-        HeaderBar,
-        Language,
-        TagsNav,
         Fullscreen,
+        // HeaderBar,
+        Language,
+        // TagsNav,
+        // Fullscreen,
         ErrorStore,
         User,
         userNotices
       },
     data () {
       return {
-        collapsed: false,
-        minLogo,
-        maxLogo,
-        isFullscreen: false
+        // collapsed: false,
+        // minLogo,
+        // maxLogo,
+        isFullscreen: false,
+        activity_name: "judge"
       }
     },
     computed: {
-      ...mapGetters([
-        'errorCount'
-      ]),
-      tagNavList () {
-        return this.$store.state.app.tagNavList
-      },
-      tagRouter () {
-        return this.$store.state.app.tagRouter
-      },
-      userAvator () {
-        return this.$store.state.user.avatorImgPath
-      },
-      cacheList () {
-        return this.tagNavList.length ? this.tagNavList.filter(item => !(item.meta && item.meta.notCache)).map(item => item.name) : []
-      },
-      menuList () {
-        return this.$store.getters.menuList
-      },
-      local () {
-        return this.$store.state.app.local
-      },
-      hasReadErrorPage () {
-        return this.$store.state.app.hasReadErrorPage
-      }
+      // ...mapGetters([
+      //   'errorCount'
+      // ]),
+      // tagNavList () {
+      //   return this.$store.state.app.tagNavList
+      // },
+      // tagRouter () {
+      //   return this.$store.state.app.tagRouter
+      // },
+      // userAvator () {
+      //   return this.$store.state.user.avatorImgPath
+      // },
+      // cacheList () {
+      //   return this.tagNavList.length ? this.tagNavList.filter(item => !(item.meta && item.meta.notCache)).map(item => item.name) : []
+      // },
+      // menuList () {
+      //   return this.$store.getters.menuList
+      // },
+      // local () {
+      //   return this.$store.state.app.local
+      // },
+      // hasReadErrorPage () {
+      //   return this.$store.state.app.hasReadErrorPage
+      // }
     },
     methods: {
-      ...mapMutations([
-        'setBreadCrumb',
-        'setTagNavList',
-        'addTag',
-        'setLocal'
-      ]),
-      ...mapActions([
-        'handleLogin'
-      ]),
-      turnToPage (route) {
-        let { name, params, query } = {}
-        if (typeof route === 'string') name = route
-        else {
-          name = route.name
-          params = route.params
-          query = route.query
-        }
-        if (name.indexOf('isTurnByHref_') > -1) {
-          window.open(name.split('_')[1])
-          return
-        }
-        this.$router.push({
-          name,
-          params,
-          query
-        })
-      },
-      handleCollapsedChange (state) {
-        this.collapsed = state
-      },
-      handleCloseTag (res, type, route) {
-        if (type === 'all') {
-          this.turnToPage(this.$config.homeName)
-        } else if (routeEqual(this.$route, route)) {
-          if (type !== 'others') {
-            const nextRoute = getNextRoute(this.tagNavList, route)
-            this.$router.push(nextRoute)
+        onMenuSelect: function(name) {
+          if (this.$route.path === '/_guider') {
+            this.$router.push('_guider/' + name);
+          } else {
+            this.$router.push(name);
+          }
+        },
+        highlightMenu() {
+          let path = this.$route.path;
+          if (path === '/_guider/judge') {
+            this.activity_name = 'judge';
+          } else if (path === '/_guider/attend') {
+            this.activity_name = 'attend';
+          } else if (path === '/_guider/consult_apply') {
+            this.activity_name = 'consult_apply';
           }
         }
-        this.setTagNavList(res)
-      },
-      handleClick (item) {
-        this.turnToPage(item)
-      }
+      // ...mapMutations([
+      //   'setBreadCrumb',
+      //   'setTagNavList',
+      //   'addTag',
+      //   'setLocal'
+      // ]),
+      // ...mapActions([
+      //   'handleLogin'
+      // ]),
+      // turnToPage (route) {
+      //   let { name, params, query } = {}
+      //   if (typeof route === 'string') name = route
+      //   else {
+      //     name = route.name
+      //     params = route.params
+      //     query = route.query
+      //   }
+      //   if (name.indexOf('isTurnByHref_') > -1) {
+      //     window.open(name.split('_')[1])
+      //     return
+      //   }
+      //   this.$router.push({
+      //     name,
+      //     params,
+      //     query
+      //   })
+      // },
+      // handleCollapsedChange (state) {
+      //   this.collapsed = state
+      // },
+      // handleCloseTag (res, type, route) {
+      //   if (type === 'all') {
+      //     this.turnToPage(this.$config.homeName)
+      //   } else if (routeEqual(this.$route, route)) {
+      //     if (type !== 'others') {
+      //       const nextRoute = getNextRoute(this.tagNavList, route)
+      //       this.$router.push(nextRoute)
+      //     }
+      //   }
+      //   this.setTagNavList(res)
+      // },
+      // handleClick (item) {
+      //   this.turnToPage(item)
+      // }
     },
     watch: {
       '$route' (newRoute) {
@@ -175,22 +200,26 @@
       }
     },
     mounted () {
-      /**
-       * @description 初始化设置面包屑导航和标签导航
-       */
-      this.setTagNavList()
-      this.addTag({
-        route: this.$store.state.app.homeRoute
-      })
-      this.setBreadCrumb(this.$route)
-      // 设置初始语言
-      this.setLocal(this.$i18n.locale)
-      // 如果当前打开页面不在标签栏中，跳到homeName页
-      if (!this.tagNavList.find(item => item.name === this.$route.name)) {
-        this.$router.push({
-          name: this.$config.homeName
-        })
-      }
+        if (this.$route.path === "/_guider") {
+          this.$router.replace("/_guider/judge");
+        }
+        this.highlightMenu();
+      // /**
+      //  * @description 初始化设置面包屑导航和标签导航
+      //  */
+      // this.setTagNavList()
+      // this.addTag({
+      //   route: this.$store.state.app.homeRoute
+      // })
+      // this.setBreadCrumb(this.$route)
+      // // 设置初始语言
+      // this.setLocal(this.$i18n.locale)
+      // // 如果当前打开页面不在标签栏中，跳到homeName页
+      // if (!this.tagNavList.find(item => item.name === this.$route.name)) {
+      //   this.$router.push({
+      //     name: this.$config.homeName
+      //   })
+      // }
     }
   }
 </script>
--- src/view/GuiderFormChoose/index.vue
+++ src/view/GuiderFormChoose/index.vue
@@ -23,43 +23,43 @@
 </template>
 
 <script>
-  import { queryFormMetas } from '../../service/api/dqs'
-  import {getCurrentTerms, queryTerms} from '../../service/api/term'
-  import form_meta_card from 'Views/components/form_meta_card/form_meta_card'
-  export default {
+import { queryFormMetas } from '../../service/api/dqs'
+import {getCurrentTerms, queryTerms} from '../../service/api/term'
+import form_meta_card from 'Views/components/form_meta_card/form_meta_card'
+export default {
   components: {form_meta_card},
   name: 'index.vue',
   data: function () {
     return {
       data: [],
       terms: [],
-      query: {},
+      query: {}
     }
   },
-  methods:{
-    onCardClick: function(name, version) {
+  methods: {
+    onCardClick: function (name, version) {
       this.$router.push({path: `/dqs/form_fill/${name}/${version}`})
-    },
+    }
   },
   mounted: function () {
+    // let args = this.$route.query
+    // queryTerms().then((resp) => {
+    //   this.terms = resp.data.terms
+    // })
+    // queryFormMetas(args).then((resp) => {
+    //   this.data = resp.data.form_metas
+    //   this.$router.push({name: '问卷选择'})
+    // })
+  },
+  onSearch (query) {
+    // 查询变化 当点提交查询条件生效
     let args = this.$route.query
-    queryTerms().then((resp) => {
-      this.terms = resp.data.terms
-    })
-    queryFormMetas(args).then((resp) => {
+    queryFormMetas({...query, args}).then((resp) => {
       this.data = resp.data.form_metas
       this.$router.push({name: '问卷选择'})
     })
-    },
-    onSearch (query) {
-      // 查询变化 当点提交查询条件生效
-      let args = this.$route.query
-      queryFormMetas({...query,args}).then((resp) => {
-        this.data = resp.data.form_metas
-        this.$router.push({name: '问卷选择'})
-      })
-    }
   }
+}
 </script>
 
 <style scoped>
