054e6afe9e71df70222f6fb326236dcd9a7fc064
mac <HeMingxu@bjfu.edu.cn>
2018-11-08 23:25:04 +0800

    改用户详情界面

--- src/view/Profile/index.vue
+++ src/view/Profile/index.vue
@@ -1,53 +1,105 @@
 <template>
-  <Row>
-    <Col span="12">
-      <Card>
-        <h1>用户详情</h1>
-        <br>
-        <Form :model="user" ref="current_user" :rules="userValidate" style="width: 50%">
-          <h3>用户名</h3>
-          <FormItem prop="username">
+  <Card>
+    <div style="width: 100%;height: 101px;text-align: center"><img src="./avatar/bjfu_logo.jpeg" style="border-radius: 50%"/></div>
+    <h1>用户详情</h1>
+    <br>
+    <Form :model="user" ref="current_user" :rules="userValidate" style="width: 50%">
+      <Row>
+        <Col span="8">
+          <FormItem prop="id" label="督导ID">
+            <Input type="text" style="width: 300px;" v-model="user.id" disabled="true"></Input>
+          </FormItem>
+        </Col>
+        <Col span="8" offset="8">
+          <FormItem prop="username" label="姓名">
             <Input type="text" style="width: 300px;" v-model="user.username"></Input>
           </FormItem>
-          <br>
-          <h3>性别</h3>
-          <FormItem>
+        </Col>
+      </Row>
+      <br>
+      <Row>
+        <Col span="8">
+          <FormItem prop="sex" label="性别">
             <Input type="text" style="width: 300px;" v-model="user.sex"></Input>
           </FormItem>
-          <br>
-          <h3>邮箱</h3>
-          <FormItem prop="email">
-            <Input type="email" style="width: 300px;" v-model="user.email"></Input>
-          </FormItem>
-          <br>
-          <h3>联系方式</h3>
-          <FormItem prop="phone">
-            <Input type="text" style="width: 300px;" v-model="user.phone"></Input>
-          </FormItem>
-          <br>
-          <h3>学院</h3>
-          <FormItem prop="unit">
+        </Col>
+        <Col span="8" offset="8">
+          <FormItem prop="unit" label="所属机构">
             <Input type="text" style="width: 300px;" v-model="user.unit"></Input>
           </FormItem>
-          <br>
-          <h3>专业</h3>
-          <FormItem prop="skill">
-            <Input type="text" style="width: 300px;" v-model="user.skill"></Input>
+        </Col>
+      </Row>
+      <br>
+      <Row>
+        <Col span="8">
+          <FormItem prop="state" label="教师状态">
+            <Input type="text" style="width: 300px;" v-model="user.state"></Input>
           </FormItem>
-          <br>
-          <h3>职称</h3>
-          <FormItem prop="prorank">
+        </Col>
+        <Col span="8" offset="8">
+          <FormItem prop="email" label="邮箱">
+            <Input type="email" style="width: 300px;" v-model="user.email"></Input>
+          </FormItem>
+        </Col>
+      </Row>
+      <br>
+      <Row>
+        <Col span="8">
+          <FormItem prop="phone" label="电话号码">
+            <Input type="text" style="width: 300px;" v-model="user.phone"></Input>
+          </FormItem>
+        </Col>
+        <Col span="8" offset="8">
+          <FormItem prop="group" label="组别">
+            <Input type="text" style="width: 300px;" v-model="user.guider.group"></Input>
+          </FormItem>
+        </Col>
+      </Row>
+      <br>
+      <Row>
+        <Col span="8">
+          <FormItem prop="status" label="签约状态">
+            <Input type="text" style="width: 300px;" v-model="user.status"></Input>
+          </FormItem>
+        </Col>
+        <Col span="8" offset="8">
+          <FormItem prop="prorank" label="职称">
             <Input type="text" style="width: 300px;" v-model="user.prorank"></Input>
           </FormItem>
-          <br>
-          <FormItem>
-            <Button type="primary">更新基本信息</Button>
+        </Col>
+      </Row>
+      <br>
+      <Row>
+        <Col span="8">
+          <FormItem prop="term" label="开始学期">
+            <Input type="text" style="width: 300px;" v-model="user.guider.term"></Input>
           </FormItem>
-        </Form>
-      </Card>
-    </Col>
-  </Row>
-
+        </Col>
+        <Col span="8" offset="8">
+          <FormItem prop="term" label="结束学期">
+            <Input type="text" style="width: 300px;" v-model="user.guider.term"></Input>
+          </FormItem>
+        </Col>
+      </Row>
+      <Row>
+        <Col span="8">
+          <FormItem prop="work_state" label="督导类型">
+            <Input type="text" style="width: 300px;" v-model="user.guider.work_state"></Input>
+          </FormItem>
+        </Col>
+        <Col span="8" offset="8">
+          <FormItem prop="skill" label="专业领域">
+            <Input type="text" style="width: 300px;" v-model="user.skill"></Input>
+          </FormItem>
+        </Col>
+      </Row>
+      <br>
+      <br>
+      <FormItem>
+        <Button type="primary">更新基本信息</Button>
+      </FormItem>
+    </Form>
+  </Card>
 </template>
 
 <script>
