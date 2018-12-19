18f5e500722103c72ecbf8218c6fdd5243fc051e
suchang <1103232282@qq.com>
2018-12-19 17:00:34 +0800

    事件选择

---
+++ src/view/UserEvents/components/event-line.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul :class="classes">
+    <slot></slot>
+  </ul>
+</template>
+<script>
+  const prefixCls = 'event-line';
+  export default {
+    name: 'Timeline',
+    props: {
+      pending: {
+        type: Boolean,
+        default: false
+      }
+    },
+    computed: {
+      classes () {
+        return [
+          `${prefixCls}`,
+          {
+            [`${prefixCls}-pending`]: this.pending
+          }
+        ];
+      }
+    }
+  };
+</script>
+<style lang="less" scoped>
+ @import './style.less';
+</style>
--- src/view/UserEvents/index.vue
+++ src/view/UserEvents/index.vue
@@ -1,29 +1,42 @@
 <template>
-  <div>
-    <h1>用户大事件</h1>
-    <Button type="primary" @click=" onSearch">增加事件</Button>
-    <div style="float: left" >
-      <Timeline v-model="events" style="width: 180px;margin-left: 20px">
-          <TimelineItem v-for="(event,index) in events" :key="'event'+index">
-            <p style="font-size: 14px; font-weight: bold;" :class="event.status">{{ event.timestamp}}</p>
-            <span @click="showTable(event, index)">
+  <Card style="height: 100%;text-align: center">
+      <h1>用户大事件</h1>
+
+      <Col span="6">
+        <div>
+          <EventLine v-model="events" style="float:left;margin-left: 20px" >
+            <EventLineItem v-for="(event,index) in events" :key="'event'+index" >
+              <div  @click="showTable(event, index)">
+                <p style="font-size: 14px; font-weight: bold;" :class="event.status" >{{ event.timestamp}}</p>
+                <span >
               <p style="font-size: 14px">{{ event.name }}</p>
               <p>{{ event.detail }}</p>
             </span>
-          </TimelineItem>
-      </Timeline>
-    </div>
-    <div style="float: left">
-      <Table stripe :columns="columns" :data="data" style="width: 860px; margin-left: 20px; margin-top: 20px"></Table>
-    </div>
-  </div>
+              </div>
+            </EventLineItem>
+          </EventLine>
+        </div>
+      </Col>
+      <Col span="18">
+        <div>
+          <Button type="primary">新增</Button>
+        </div>
+        <div>
+          <Table stripe :columns="columns" :data="data" style="width: 860px; margin-left: 20px; margin-top: 20px"></Table>
+        </div>
+      </Col>
+  </Card>
 </template>
 
 <script>
   import { postEvents } from '@/service/api/events'
   import { getEvents } from '@/service/api/user'
 import { queryForms, getForm } from '@/service/api/dqs'
-export default {
+  import EventLine from './components/event-line'
+  import EventLineItem from './components/event-line-item'
+
+  export default {
+    components:{EventLine, EventLineItem},
   data: function () {
     return {
       events: [],
