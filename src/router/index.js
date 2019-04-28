535a781fbd0be399cad7891204bcfec3098b6f82
suchang <1103232282@qq.com>
2019-04-28 18:19:34 +0800

    基本ok

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,16 @@ import Router from 'vue-router'
 import routes from './routers'
 import store from '@/store'
 import iView from 'iview'
-import {currentUser} from '../service/api/user'
+import { currentUser } from '../service/api/user'
 import { setToken, getToken, canTurnTo } from '@/libs/util'
 import config from '@/config'
-import {ROUTER_DEFAULT_CONFIG} from '@/config/config'
+import { ROUTER_DEFAULT_CONFIG } from '@/config/config'
 const { homeName } = config
 
 Vue.use(Router)
 const router = new Router({
   routes,
-  mode: 'history',
+  mode: 'hash',
   ...ROUTER_DEFAULT_CONFIG
 })
 const LOGIN_PAGE_NAME = 'login'
@@ -22,27 +22,25 @@ const turnTo = (to, access, next) => {
   else next({ replace: true, name: 'error_401' }) // 无权限，重定向到401页面
 }
 
-
 router.beforeEach((to, from, next) => {
   iView.LoadingBar.start()
   // const token = getToken()
   if (to.name !== LOGIN_PAGE_NAME) {
-
     currentUser().then((resp) => {
       store.commit('setUserInfo', resp.data.current_user)
       if (to.path === '/') {
         if (resp.data.current_user.is_guider) {
-          next({name: '督导端'})
+          next({ name: '督导端' })
         }
-        next({name: 'home'})
+        next({ name: 'home' })
       }
       next()
     }).catch(() => {
-      next({path: '/login'})
+      next({ path: '/login' })
     })
   } else {
     if (to.path === '/') {
-      next({path: '/home'})
+      next({ path: '/home' })
     }
     next()
   }
