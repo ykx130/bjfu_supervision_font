ae79c7fc3ba2e2cd6b33d755b7bb17e6fd59ed9a
suchang <1103232282@qq.com>
2019-05-03 11:39:24 +0800

    完善评价用户

--- src/view/components/form_show/lesson_meta_form.vue
+++ src/view/components/form_show/lesson_meta_form.vue
@@ -1,16 +1,15 @@
-
 <template>
   <div>
     <Form :model="value" :label-width="80" label-position="left">
       <Row :gutter="16">
         <Col span="6">
-          <FormItem label="章节目录" :required="true" >
+          <FormItem label="章节目录" :required="true">
             <Input v-model="value.lesson.content" class="inline-form-item" :disabled="disabled"> </Input>
           </FormItem>
         </Col>
         <Col span="6">
           <FormItem label="听课督导" :required="true">
-            <Select :value="value.guider"
+            <Select v-model="value.guider"
                     filterable
                     clearable
                     remote
@@ -19,20 +18,22 @@
                     @on-change="onGuiderSelectChange"
                     :disabled="guider_disable || disabled"
                     class="inline-form-item">
-              <Option v-for="(item, key) in users" :value="item.username" :key="item.username + item.name + item.id"   >{{'     ' + item.username + '     '}}</Option>
+              <Option v-for="(item, key) in users" :value="item.username" :key="item.username + item.id">
+                {{"'" + item.username + "'"}}
+              </Option>
             </Select>
           </FormItem>
         </Col>
         <Col span="6">
-          <FormItem  label="听课学期" prop="term" >
-            <Select v-model="value.term"  @on-change="onTermSelectChange" class="inline-form-item" :disabled="disabled">
+          <FormItem label="听课学期" prop="term">
+            <Select v-model="value.term" @on-change="onTermSelectChange" class="inline-form-item" :disabled="disabled">
               <Option v-for="item in terms" :value="item.name" :key="item.name">{{ item.name }}</Option>
             </Select>
           </FormItem>
         </Col>
         <Col span="6">
-          <FormItem  label="听课级别" prop="lesson.lesson_level">
-            <Input   v-model="value.lesson.lesson_level" disabled class="inline-form-item"></Input>
+          <FormItem label="听课级别" prop="lesson.lesson_level">
+            <Input v-model="value.lesson.lesson_level" disabled class="inline-form-item"></Input>
           </FormItem>
         </Col>
       </Row>
@@ -40,7 +41,7 @@
     </Form>
 
     <Form :model="value">
-      <table class="table-border meta-table" >
+      <table class="table-border meta-table">
         <tbody>
         <tr>
           <th>课程名字</th>
@@ -61,31 +62,32 @@
                       @on-change="onSelectedLessonChange"
                       :disabled="disabled"
                       filterable>
-                <Option v-for="(item,index) in lessons" :value="item.lesson_id" :key="item.lesson_id + item.lesson_name + item.id">
-                  {{ '     '+item.lesson_name+'___' + item.lesson_teacher_name+ '___'+item.lesson_class+'___'+'     '}}
+                <Option v-for="(item,index) in lessons" :value="item.lesson_id"
+                        :key="item.lesson_id + item.lesson_name + item.id">
+                  {{ "'"+item.lesson_name+'___' + item.lesson_teacher_name+ '___'+item.lesson_class+'___'+"'"}}
                 </Option>
               </Select>
             </FormItem>
           </td>
           <td>
-            <FormItem  :required="true" class="table-form-item">
+            <FormItem :required="true" class="table-form-item">
               <Input v-model="value.lesson.lesson_teacher_name"
                      :disabled="disabled"></Input>
             </FormItem>
           </td>
           <td>
-            <FormItem  :required="true" class="table-form-item">
-              <Input v-model="value.lesson.lesson_class" :disabled="disabled" ></Input>
+            <FormItem :required="true" class="table-form-item">
+              <Input v-model="value.lesson.lesson_class" :disabled="disabled"></Input>
             </FormItem>
           </td>
           <td>
             <FormItem :required="true" class="table-form-item">
-              <DatePicker  width="100%" type="date"
-                           :value="value.lesson.lesson_date"
-                           format="yyyy-MM-dd"
-                           @on-change="onSelectedLessonCaseChange"
-                           :options="getLessonDatePickerOption()"
-                           :disabled="disabled"></DatePicker>
+              <DatePicker width="100%" type="date"
+                          :value="value.lesson.lesson_date"
+                          format="yyyy-MM-dd"
+                          @on-change="onSelectedLessonCaseChange"
+                          :options="getLessonDatePickerOption()"
+                          :disabled="disabled"></DatePicker>
             </FormItem>
           </td>
           <td>
@@ -94,12 +96,13 @@
             </FormItem>
           </td>
           <td>
-            <FormItem  :required="true" class="table-form-item">
-              <Select v-model="value.lesson.lesson_times"  multiple :disabled="disabled">
+            <FormItem :required="true" class="table-form-item">
+              <Select v-model="value.lesson.lesson_times" multiple :disabled="disabled">
 
                 <Option v-for="item in lesson_times"
                         :value="item.value"
-                        :key="item.value + item.key">{{ item.label }}</Option>
+                        :key="item.value + item.key">{{ item.label }}
+                </Option>
               </Select>
             </FormItem>
           </td>
@@ -116,6 +119,7 @@ import { querySupervisors } from '@/service/api/user'
 import { dateToString } from 'Libs/tools'
 import { queryTerms, getCurrentTerms } from '@/service/api/term'
 import { transTimeToSelectedData } from 'Libs/tools'
+
 export default {
   props: {
     value: {
@@ -169,7 +173,7 @@ export default {
           this.selected_lesson = resp.data.lesson
           // 处理case
           this.lessons[this.selected_lesson.lesson_id] = this.selected_lesson
-          this.value.lesson.lesson_id  = this.selected_lesson.lesson_id
+          this.value.lesson.lesson_id = this.selected_lesson.lesson_id
           this.value.lesson.lesson_date = undefined
           this.lesson_times = []
           this.allow_select_data = this.selected_lesson.lesson_cases.map((item) => {
@@ -243,14 +247,14 @@ export default {
     },
 
     onGuiderQueryChange: function (value) {
-      if (!(value.startWith('     ') &&value.endsWith('     '))) {
+      if (!(value.startsWith("'") && value.endsWith("'"))) {
         this.guider_name_like = value
         this.fetchUser()
       }
     },
 
     onLessonQueryChange: function (value) {
-      if (!(value.startWith('     ')&&value.endsWith('     '))) {
+      if (!(value.startsWith("'") && value.endsWith("'"))) {
         this.lesson_name_like = value
         this.fetchLesson()
       }
@@ -260,12 +264,18 @@ export default {
       if (value) {
         this.select_guider = this.users[value]
         if (this.select_guider) {
-          this.value.guider_name = this.select_guider.name
-          this.value.guider_group = this.select_guider.group
+          this.value = {
+            ...this.value,
+            guider_name: this.select_guider.user.name,
+            guider_group: this.select_guider.group
+          }
         }
       } else {
-        this.value.guider_name = undefined
-        this.value.guider_group = undefined
+        this.value = {
+          ...this.value,
+          guider_name: undefined,
+          guider_group: undefined
+        }
       }
     },
 
@@ -330,7 +340,9 @@ export default {
       return {
         disabledDate (value) {
           let d = dateToString(value, 'yyyy-MM-dd')
-          let flag = self.allow_select_data.findIndex((item) => { return item === d })
+          let flag = self.allow_select_data.findIndex((item) => {
+            return item === d
+          })
           return (flag === -1)
         }
       }
@@ -345,31 +357,36 @@ export default {
 <style scoped lang="less">
 
   .table-border {
-    border:1px solid #ddd;
+    border: 1px solid #ddd;
     border-collapse: collapse;
     border-spacing: 0;
-    tbody>tr{
-      text-align:left;
-      border:1px solid #ddd;
-    };
-    tbody>tr>th {
+    tbody > tr {
+      text-align: left;
+      border: 1px solid #ddd;
+    }
+  ;
+    tbody > tr > th {
       padding: 8px;
-      border:1px solid #ddd;
-    };
-    tbody>tr>td {
+      border: 1px solid #ddd;
+    }
+  ;
+    tbody > tr > td {
       padding: 8px;
-      border:1px solid #ddd;
+      border: 1px solid #ddd;
       height: 30px;
-    };
+    }
+  ;
     .table-form-item {
       width: 160px;
-      vertical-align:middle;
+      vertical-align: middle;
       margin-bottom: 3px;
     }
   }
-  .meta-table{
+
+  .meta-table {
     width: 100%;
   }
+
   .inline-form-item {
     width: 184px;
   }
