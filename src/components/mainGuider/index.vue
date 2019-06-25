4023d29dce868c38735cc06e12e1154afda0393e
suchang <1103232282@qq.com>
2019-06-25 15:26:21 +0800

    课程

--- src/components/mainGuider/index.vue
+++ src/components/mainGuider/index.vue
@@ -45,7 +45,7 @@
             <template v-permission="['督导','管理员']">
               <MenuItem name="judge">
                 <Icon type="ios-paper" />
-                问卷填写
+                评价填写
               </MenuItem>
               <MenuItem name="lesson_form">
                 <Icon type="ios-construct" />
@@ -53,7 +53,7 @@
               </MenuItem>
               <MenuItem name="my_form">
                 <Icon type="ios-construct" />
-                我的问卷
+                我的评价
               </MenuItem>
             </template>
 
@@ -82,130 +82,129 @@
   </div>
 </template>
 <script>
-  import HeaderBar from '_c/main/components/header-bar'
-  import User from '_c/mainGuider/components/user'
-  import Fullscreen from '_c/mainGuider/components/fullscreen'
-  import Language from '_c/mainGuider/components/language'
-  import userNotices from '_c/user_notices'
-  import ErrorStore from '_c/mainGuider/components/error-store'
-  import { mapMutations, mapActions, mapGetters } from 'vuex'
+import HeaderBar from '_c/main/components/header-bar'
+import User from '_c/mainGuider/components/user'
+import Fullscreen from '_c/mainGuider/components/fullscreen'
+import Language from '_c/mainGuider/components/language'
+import userNotices from '_c/user_notices'
+import ErrorStore from '_c/mainGuider/components/error-store'
+import { mapMutations, mapActions, mapGetters } from 'vuex'
 
-
-  export default {
-      name: 'mainGuider',
-      components: {
-        Fullscreen,
-        // HeaderBar,
-        Language,
-        // TagsNav,
-        //Fullscreen,
-        ErrorStore,
-        User,
-        userNotices
-      },
-    data () {
-      return {
-        // collapsed: false,
-        // minLogo,
-        // maxLogo,
-        isFullscreen: false,
-        activity_name: "judge"
+export default {
+  name: 'mainGuider',
+  components: {
+    Fullscreen,
+    // HeaderBar,
+    Language,
+    // TagsNav,
+    // Fullscreen,
+    ErrorStore,
+    User,
+    userNotices
+  },
+  data () {
+    return {
+      // collapsed: false,
+      // minLogo,
+      // maxLogo,
+      isFullscreen: false,
+      activity_name: 'judge'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'errorCount'
+    ]),
+    tagNavList () {
+      return this.$store.state.app.tagNavList
+    },
+    tagRouter () {
+      return this.$store.state.app.tagRouter
+    },
+    userAvator () {
+      return this.$store.state.user.avatorImgPath
+    },
+    cacheList () {
+      return this.tagNavList.length ? this.tagNavList.filter(item => !(item.meta && item.meta.notCache)).map(item => item.name) : []
+    },
+    menuList () {
+      return this.$store.getters.menuList
+    },
+    local () {
+      return this.$store.state.app.local
+    },
+    hasReadErrorPage () {
+      return this.$store.state.app.hasReadErrorPage
+    }
+  },
+  methods: {
+    onMenuSelect: function (name) {
+      if (this.$route.path === '/_guider') {
+        this.$router.push({ path: '/_guider/' + name })
+      } else {
+        this.$router.push({ path: '/_guider/' + name })
       }
     },
-    computed: {
-      ...mapGetters([
-        'errorCount'
-      ]),
-      tagNavList () {
-        return this.$store.state.app.tagNavList
-      },
-      tagRouter () {
-        return this.$store.state.app.tagRouter
-      },
-      userAvator () {
-        return this.$store.state.user.avatorImgPath
-      },
-      cacheList () {
-        return this.tagNavList.length ? this.tagNavList.filter(item => !(item.meta && item.meta.notCache)).map(item => item.name) : []
-      },
-      menuList () {
-        return this.$store.getters.menuList
-      },
-      local () {
-        return this.$store.state.app.local
-      },
-      hasReadErrorPage () {
-        return this.$store.state.app.hasReadErrorPage
+    highlightMenu () {
+      let path = this.$route.path
+      if (path === '/_guider/judge') {
+        this.activity_name = 'judge'
+      } else if (path === '/_guider/attend') {
+        this.activity_name = 'attend'
+      } else if (path === '/_guider/consult_apply') {
+        this.activity_name = 'consult_apply'
+      } else if (path === '/_guider/lesson_form') {
+        this.activity_name = 'lesson_form'
       }
     },
-    methods: {
-        onMenuSelect: function(name) {
-          if (this.$route.path === '/_guider') {
-            this.$router.push({path:'/_guider/' + name});
-          } else {
-            this.$router.push({path:'/_guider/' + name});
-          }
-        },
-        highlightMenu() {
-          let path = this.$route.path;
-          if (path === '/_guider/judge') {
-            this.activity_name = 'judge';
-          } else if (path === '/_guider/attend') {
-            this.activity_name = 'attend';
-          } else if (path === '/_guider/consult_apply') {
-            this.activity_name = 'consult_apply';
-          } else if (path === '/_guider/lesson_form'){
-            this.activity_name = 'lesson_form'
-          }
-        },
-      ...mapMutations([
-        'setBreadCrumb',
-        'setTagNavList',
-        'addTag',
-        'setLocal'
-      ]),
-      ...mapActions([
-        'handleLogin'
-      ]),
-      turnToPage (route) {
-        let { name, params, query } = {}
-        if (typeof route === 'string') name = route
-        else {
-          name = route.name
-          params = route.params
-          query = route.query
-        }
-        if (name.indexOf('isTurnByHref_') > -1) {
-          window.open(name.split('_')[1])
-          return
-        }
-        this.$router.push({
-          name,
-          params,
-          query
-        })
-      },
-      handleClickToAdmin: function () {
-        this.$router.push({name:"home"})
+    ...mapMutations([
+      'setBreadCrumb',
+      'setTagNavList',
+      'addTag',
+      'setLocal'
+    ]),
+    ...mapActions([
+      'handleLogin'
+    ]),
+    turnToPage (route) {
+      let { name, params, query } = {}
+      if (typeof route === 'string') name = route
+      else {
+        name = route.name
+        params = route.params
+        query = route.query
       }
-    },
-    watch: {
-      '$route' (newRoute) {
-        this.highlightMenu()
+      if (name.indexOf('isTurnByHref_') > -1) {
+        window.open(name.split('_')[1])
+        return
       }
+      this.$router.push({
+        name,
+        params,
+        query
+      })
     },
-    mounted () {
-        if (this.$route.path === "/_guider") {
-          this.$router.replace("/_guider/judge");
-        }
-        this.highlightMenu();
-      /**
+    handleClickToAdmin: function () {
+      this.$router.push({ name: 'home' })
+    }
+  },
+  watch: {
+    '$route' (newRoute) {
+      this.highlightMenu()
+    }
+  },
+  mounted () {
+    if (this.$route.path === '/_guider') {
+      this.$router.replace('/_guider/judge')
+    }
+    this.highlightMenu()
+    /**
        * @description 初始化设置面包屑导航和标签导航
        */
-      this.setBreadCrumb(this.$route)
-      // 设置初始语言
-      this.setLocal(this.$i18n.locale)
-      // 如果当前打开页面不在标签栏中，跳到homeName页
-    }
+    this.setBreadCrumb(this.$route)
+    // 设置初始语言
+    this.setLocal(this.$i18n.locale)
+    // 如果当前打开页面不在标签栏中，跳到homeName页
   }
+}
 </script>
