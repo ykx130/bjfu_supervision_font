d2ebe03442888e856b4d559041676a3413dc0e84
suchang <1103232282@qq.com>
2019-09-10 22:19:42 +0800

    好评课堂

--- src/plugin/axios.js
+++ src/plugin/axios.js
@@ -1,18 +1,21 @@
 import axios from 'axios'
-import {AXIOS_DEFAULT_CONFIG} from 'Config/config'
-import {requestSuccessFunc, requestFailFunc, responseSuccessFunc, responseFailFunc} from '../config/interceptors/axios'
+import { AXIOS_DEFAULT_CONFIG } from 'Config/config'
+import { requestSuccessFunc, requestFailFunc, responseSuccessFunc, responseFailFunc } from '../config/interceptors/axios'
 import store from '@/store'
-
 
 let axiosInstance = {}
 axiosInstance = axios.create(AXIOS_DEFAULT_CONFIG)
 
 // 注入请求拦截
 axiosInstance
-  .interceptors.request.use((requestObj)=>{
-      console.log(store.state.user)
-  return requestObj
-}, requestFailFunc)
+  .interceptors.request.use((requestObj) => {
+    console.log(store.state.user)
+    if (store.state.user && store.state.user.access.length) {
+      requestObj.headers['CurrentRole'] = store.state.user.access[0]
+    }
+    // debugger
+    return requestObj
+  }, requestFailFunc)
 // 注入失败拦截
 axiosInstance
   .interceptors.response.use(responseSuccessFunc, responseFailFunc)
