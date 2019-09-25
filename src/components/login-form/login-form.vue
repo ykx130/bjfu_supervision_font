1d21721a1360f17e6bf376fa5bf8f539d0778f01
suchang <1103232282@qq.com>
2019-09-25 12:08:23 +0800

    验证码

--- src/components/login-form/login-form.vue
+++ src/components/login-form/login-form.vue
@@ -15,13 +15,10 @@
       </Input>
     </FormItem>
     <FormItem prop="code" style="display: flex;">
-      <Input type="code" size="large" maxlength="4"  v-model="form.code" placeholder="验证码"
-             style="width: 90px;height: 40px;position: relative;bottom: 10px" >
-      </Input>
-      <img style="margin-left: 10px;" :src="'/api/'+captcha_url" @click="refreshCaptcha()" width="80px" height="40px"/>
+      <div id="your-dom-id" class="nc-container"></div> <!--No-Captcha渲染的位置，其中 class 中必须包含 nc-container-->
     </FormItem>
     <FormItem>
-      <Button @click="handleSubmit" type="primary" long>登录</Button>
+      <Button :disabled="disable_login" @click="handleSubmit" type="primary" long>登录</Button>
     </FormItem>
   </Form>
 </template>
@@ -53,11 +50,11 @@ export default {
   data () {
     return {
       captcha_url: '',
+      nc: undefined,
+      disable_login: true,
       form: {
         userName: 'super_admin',
         password: '',
-        code: '',
-        uuid: ''
       }
     }
   },
@@ -71,9 +68,44 @@ export default {
   },
   methods: {
     refreshCaptcha: function () {
-      getCaptcha().then((resp) => {
-        this.form.uuid = resp.data.uuid
-        this.captcha_url = resp.data.path
+      var nc_token = ["FFFF0N000000000084BF", (new Date()).getTime(), Math.random()].join(':');
+      var NC_Opt =
+        {
+          renderTo: "#your-dom-id",
+          appkey: "FFFF0N000000000084BF",
+          scene: "nc_login",
+          token: nc_token,
+          customWidth: 270,
+          trans:{"key1":"code0"},
+          elementID: ["usernameID"],
+          is_Opt: 0,
+          language: "cn",
+          isEnabled: true,
+          timeout: 3000,
+          times:5,
+          apimap: {
+            // 'analyze': '//a.com/nocaptcha/analyze.jsonp',
+            // 'get_captcha': '//b.com/get_captcha/ver3',
+            // 'get_captcha': '//pin3.aliyun.com/get_captcha/ver3'
+            // 'get_img': '//c.com/get_img',
+            // 'checkcode': '//d.com/captcha/checkcode.jsonp',
+            // 'umid_Url': '//e.com/security/umscript/3.2.1/um.js',
+            // 'uab_Url': '//aeu.alicdn.com/js/uac/909.js',
+            // 'umid_serUrl': 'https://g.com/service/um.json'
+          },
+          callback:  (data) => {
+            this.disable_login = false
+            window.console && console.log(nc_token)
+            window.console && console.log(data.csessionid)
+            window.console && console.log(data.sig)
+          }
+        }
+      this.nc = new noCaptcha(NC_Opt)
+      this.nc.upLang('cn', {
+        _startTEXT: "请按住滑块，拖动到最右边",
+        _yesTEXT: "验证通过",
+        _error300: "哎呀，出错了，点击<a href=\"javascript:__nc.reset()\">刷新</a>再来一次",
+        _errorNetwork: "网络不给力，请<a href=\"javascript:__nc.reset()\">点击刷新</a>",
       })
     },
     handleSubmit () {
@@ -82,8 +114,6 @@ export default {
           this.$emit('on-success-valid', {
             userName: this.form.userName,
             password: this.form.password,
-            code: this.form.code,
-            uuid: this.form.uuid
           })
         }
       })
